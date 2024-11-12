b85be886f69c59acd7ff0854f79b2c1ac0c38882
syedfarzan <farzan639@gmail.com>
2024-11-12 15:12:22 +0500

    Nested if

---
+++ class4/list_practice.dart
@@ -0,0 +1,20 @@
+//nested if
+void main(){
+
+  int number = 25;
+
+  if (number%5==0 || number%7==0) {
+    if (number%5==0) {
+      print("number is divisible by 5");
+    }
+    else if(number%7==0){
+      print("number is divisible by 7");
+    } 
+    else {
+      print("number is divisible by both");
+    }
+  } else {
+    print("number is not divisible by 5 or 7");
+  }
+
+}

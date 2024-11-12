0516d267bdeb636c45272f5d790e20782d5dee9d
syedfarzan <farzan639@gmail.com>
2024-11-12 14:17:10 +0500

    dart Practice

---
+++ bool_condition.dart
@@ -0,0 +1,117 @@
+// void main(){
+//   bool abc = 12<8;
+//   print(abc);
+// }
+
+
+// Operators
+// 1. Arthemticatic Operators
+
+// void main(){
+//   int num1 =  5;
+//   int num2 = 2;
+//   num result = num1/num2;
+//   print(result);
+// }
+
+//divide Integer
+// void main(){
+//   int num1 =  5;
+//   int num2 = 2;
+//   num result = num1~/num2;
+//   print(result);
+// }
+
+//remainder
+
+// void main(){
+//   int num1 =  19;
+//   int num2 = 4;
+//   num result = num1%num2;
+//   print(result);
+// }
+
+//2. Equality and Relational Operators
+
+// void main(){
+//   int num1 =  19;
+//   int num2 = 4;
+//   bool result = num1 > num2;
+//   print(result);
+// }
+
+
+
+//Logical Operators 
+
+// && Operator (returns true if all the conditions are true)
+// void main(){
+//   int age =  16;
+//   bool result = age <n = 18;
+//   print(result);
+// }
+
+// // || Operator (returns true if any condition is true)
+// void main(){
+//   int age =  16;
+//   bool result = age <= 18;
+//   print(result);
+// }
+
+
+// void main(){
+//   String email = "farzan@gmail.com";
+//   String password = "12345";
+//   bool condition = email =="farzan@gmail.com" && password == "12345";
+//   print(condition);
+// }
+
+// void main(){
+//   int abc = 34;
+//   int a = 21;
+//   int q = 12;
+//   int k = 6;
+//   int i = a-q; // i = 21-12 = 9
+//   bool w = abc>a && !(a<abc);
+//   //       true  &&  false
+//   // w = false
+//   bool e = !w || q!=k;
+//   //     true || true
+//   // e = true
+//   bool o = e && i>4;
+//   //      true && true
+//   // 0 = true 
+//   print (!(!o || e));
+//   //       !(false || true)
+//   //       !(true)
+//   // answer =  false;
+// }
+
+
+// void main(){
+//   int num1 = 12;
+//   int num2 = 12;
+//   if (num1>num2){
+//     print("num1 is greater than num2");
+//   }
+//   else if(num1==num2){
+//     print("num1 is equal to num2");
+//   }
+//   else{
+//     print("num1 is smaller than num2");
+//   }
+
+//   print("if else is finished, its running after that");
+// }
+
+
+// void main(){
+//   String email = "farzan@gmail.com";
+//   String password = "12345";
+//   if (email == "farzan@gmail.com" && password == "12345"){
+//     print("Login Successful");
+//   }
+//   else{
+//     print("login failed");
+//   }
+// }
